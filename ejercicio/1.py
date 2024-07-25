135b8acdc363f02eec13120427e4e1e90ae81d80
oleon2001 <oswaldoleon72@gmail.com>
2024-07-25 16:35:15 -0400

    f

--- ejercicio/1.py
+++ ejercicio/1.py
@@ -1,12 +1,24 @@
 import pandas as pd
 import numpy as np
+import charts as ch
 
-
-df=pd.read_csv("ejercicio/Electric_Vehicle_Population_Data.csv")
+df=pd.read_csv("/home/oleon/Escritorio/platzi/ccvPrueba/ejercicio/Electric_Vehicle_Population_Data.csv")
 '''
 df.head()
 df.info()
 df.describe()
 df.isnull().sum()
 '''
-df.head()
+#print(df.head())
+
+
+#funciones
+df=df[df['city']=='Everett']
+countries=df['city'].values
+utilidadElectrica= df['Electric Utility 2020 Census Tract'].values
+
+
+
+
+
+
---
+++ ejercicio/charts2.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+
+def grafica_pie_():
+    
