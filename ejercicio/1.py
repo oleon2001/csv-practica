e85be80459168e1ea93b95216d346aed0c5c095e
oleon2001 <oswaldoleon72@gmail.com>
2024-07-25 14:40:48 -0400

    starting exercise

---
+++ ejercicio/1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+df=pd.read_csv("ejercicio/Electric_Vehicle_Population_Data.csv")
+'''
+df.head()
+df.info()
+df.describe()
+df.isnull().sum()
+'''
+df.head()
