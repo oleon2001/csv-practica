e81e62f6f3775e883b315ff253a717cdcb67ee9c
oleon2001 <oswaldoleon72@gmail.com>
2024-08-05 07:47:40 -0400

    aaaaaaaaaa

--- ejercicio_ventas/main.py
+++ ejercicio_ventas/main.py
@@ -4,19 +4,19 @@ import pandas as pd
 import charts as ch
 
 def run():
-    # Create a sample dataframe
     df = pd.read_csv(r'ejercicio_ventas/ventas.csv')
     df['total_ventas']=df.apply(lambda x: x['Cantidad'] * x['Precio_Unitario'], axis=1)
     sumastotales=df['total_ventas'].agg(['min','max'])
-    print(df)
-    print(sumastotales)
+    #print(df)
+    #print(sumastotales)
+    print(df['total_ventas'].sum())
     
     labels = []
     for i in range(len(df)):
         labels.append(df.index[i])
     
     values = df['total_ventas']
-    ch.generateBarChart(labels=labels, values=values)
+    #ch.generateBarChart(labels=labels, values=values)
 
 if __name__ == '__main__':
     run() 
